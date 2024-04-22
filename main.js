ee9ef0d7f5bbfd73ee27fda0de44f37ae49547e9
Eric White <led.ecodyswhite@gmail.com>
2024-04-21 17:25:09 -0700

    sorts your pixels (now without console.logs)

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//current step: add pixels to new canvas from sortedHexArr
-
 function rgbToHex(r, g, b) {
   //code found in the deepest depths of purgatory itself
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
@@ -19,13 +17,10 @@ image.onload = function () {
 
   canvas.style.backgroundColor = "green";
   let imgData = ctx.getImageData(0, 0, image.width, image.height).data;
-  let rgbArr = imgData.filter(function (x) {
-    if ((imgData[x] + 1) % 4 != 0) {
-      return x;
-    }
-  });
-//   console.log(rgbArr.length / imgData.length);
-//this still comes out wrong but the other numbers are good idk what's up with that.
+  let rgbArr = [];
+  for (let i = 0; i < imgData.length; i += 4) {
+    rgbArr.push(imgData[i], imgData[i + 1], imgData[i + 2]);
+  }
   hexArr = [];
   tempArr = [];
   for (let i = 0; i < rgbArr.length; i++) {
@@ -33,27 +28,31 @@ image.onload = function () {
       hexArr.push(rgbToHex(tempArr[0], tempArr[1], tempArr[2]));
       tempArr = [];
     }
-    tempArr.push(i);
+    tempArr.push(rgbArr[i]);
   }
   hexArr.push(rgbToHex(tempArr[0], tempArr[1], tempArr[2]));
   tempArr = [];
-//   console.log(hexArr);
   let sortedHexArr = hexArr.sort();
-//   console.log(sortedHexArr);
-//   console.log(rgbArr.length)
   let oopsArr = hexArr.filter(function (x) {
-    return x.length < 6;
+    return x.length > 7;
   });
-  console.log(oopsArr);
-  //why are there hex values that are not six digits long (7 with #)????
-  //it doesn't make sense how did those get there
+
+  let pixelSorted = document.getElementById("pixelSorted");
+
+  pixelSorted.height = image.height;
+  pixelSorted.width = image.width;
+
+  let ctx2 = pixelSorted.getContext("2d");
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < sortedHexArr.length; i++) {
+    if (x == pixelSorted.width) {
+      x = 0;
+      y++;
+    }
+    ctx2.fillStyle = sortedHexArr[i];
+    ctx2.fillRect(x, y, 1, 1);
+    x++;
+  }
 };
-
-let pixelSorted = document.getElementById("pixelSorted")
-
-pixelSorted.height = image.height
-pixelSorted.width = image.width
-
-let ctx = pixelSorted.getContext("2d");
-let drawX = 0
-let drawY = 0
